37410a4053f250b5cd8e8fda0cfd4887a5b97681
orlovssky <orlovssky@protonmail.com>
2023-05-23 17:50:36 +0400

    clean

--- src/App.tsx
+++ src/App.tsx
@@ -1,87 +1,31 @@
-import { ScrollTrigger } from 'gsap/ScrollTrigger'
-import { gsap } from 'gsap'
-import { useEffect } from 'react'
+import { MouseEventHandler } from 'react'
 
 import './App.scss'
 
 const App = () => {
-  useEffect(() => {
-    gsap.registerPlugin(ScrollTrigger)
-
-    const indexImages: string[] = []
-
-    for (let i = 1; i <= 370; i++) {
-      indexImages.push(i.toString())
-    }
-
-    const loadImages = () => {
-      return Promise.all(
-        indexImages.map((i) => import(`./assets/images/rocket/rocket_${i}.jpg`))
+  const handleMouseMove: MouseEventHandler = (event) => {
+    if (event.target instanceof HTMLSpanElement) {
+      const {
+        target: { offsetLeft, offsetTop },
+        pageX,
+        pageY,
+      } = event
+      const halfWidth = event.target.offsetWidth / 2
+      const halfHeight = event.target.offsetHeight / 2
+      const positionToCenter = Math.floor(
+        Math.sqrt(
+          Math.pow(pageX - (offsetLeft + halfWidth), 2) +
+            Math.pow(pageY - (offsetTop + halfHeight), 2)
+        )
       )
+
+      console.log(Math.floor((100 * positionToCenter) / halfWidth))
     }
-
-    loadImages().then((loadedImages) => {
-      const canvas = document.querySelector('canvas')
-      const top = document.querySelector('.canvas')
-      const context = canvas?.getContext('2d')
-
-      if (canvas && context) {
-        canvas.width = window.innerWidth
-        canvas.height = window.innerHeight
-        const images: HTMLImageElement[] = []
-        const airpods = {
-          frame: 0,
-        }
-
-        for (const image of loadedImages) {
-          const img = new Image()
-          img.src = image.default
-          images.push(img)
-        }
-
-        const render = () => {
-          context.clearRect(0, 0, canvas.width, canvas.height)
-          context.drawImage(
-            images[airpods.frame],
-            0,
-            0,
-            canvas.width,
-            canvas.height
-          )
-        }
-
-        gsap
-          .timeline({
-            onUpdate: render,
-            scrollTrigger: {
-              trigger: top,
-              scrub: 0.5,
-              end: '+=400',
-            },
-          })
-          .to(
-            airpods,
-            {
-              frame: 370 - 1,
-              snap: 'frame',
-              ease: 'none',
-              duration: 1,
-            },
-            0
-          )
-
-        ScrollTrigger.create({ trigger: 'canvas', end: '+=400' })
-        images[images.length - 1].onload = render
-      }
-    })
-  }, [])
+  }
 
   return (
     <>
-      <div className="vh-100" />
-      <canvas />
-      <div className="canvas" />
-      <div className="height-400" />
+      <span onMouseMove={handleMouseMove}>basketball</span>
     </>
   )
 }
